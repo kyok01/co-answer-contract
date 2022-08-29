3f59792958e14bfec1366c203474463480cc3080
kyok01 <kazutakayokoils@gmail.com>
2022-08-29 16:08:35 +0900

    deploy function about answer

--- contracts/NFT.sol
+++ contracts/NFT.sol
@@ -6,57 +6,145 @@ import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
+import "@openzeppelin/contracts/utils/Strings.sol";
+import "@openzeppelin/contracts/utils/Base64.sol";
 
 import "hardhat/console.sol";
 
 contract CoAnswer is ERC721, ERC721Enumerable, ERC721URIStorage, Ownable {
     using Counters for Counters.Counter;
+    using Strings for uint256;
 
     Counters.Counter private _tokenIdCounter;
+    Counters.Counter private _answerIdCounter;
 
-    uint MINT_PRICE = 0.001 ether;
+    uint256 MINT_PRICE = 0.001 ether;
 
-    struct ListedToken {
-        uint256 tokenId;
+    struct Props {
         address payable owner;
-        // uint256 price;
-        // bool currentlyListed;
+        string question;
     }
 
-     //This mapping maps tokenId to token info and is helpful when retrieving details about a tokenId
-    mapping(uint256 => ListedToken) idToListedToken;
+    struct Answer {
+        uint256 tokenId;
+        uint256 answerId;
+        address payable sender;
+        string answerText;
+    }
+
+    struct Answers {
+        Answer[] answers;
+    }
+
+    //This mapping maps tokenId to token info and is helpful when retrieving details about a tokenId
+    mapping(uint256 => Props) idToProps;
+    mapping(uint256 => Answers) idToAnswers;
 
     constructor() ERC721("CoAnswer", "CAS") {}
 
-    function safeMint(address to, string memory uri) public payable {
+    /**
+     * function about mint QuestionNft
+     */
+    function safeMint(address to, string memory question) public payable {
         require(msg.value == MINT_PRICE);
         uint256 tokenId = _tokenIdCounter.current();
         _tokenIdCounter.increment();
         _safeMint(to, tokenId);
-        _setTokenURI(tokenId, uri);
 
-        idToListedToken[tokenId] = ListedToken(
-            tokenId,
-            payable(msg.sender)
+        idToProps[tokenId] = Props(payable(msg.sender), question);
+
+        _setTokenURI(tokenId, getTokenURI(tokenId));
+    }
+
+    function getPropsForId(uint256 tokenId) public view returns (Props memory) {
+        return idToProps[tokenId];
+    }
+
+    function generateImage(uint256 tokenId)
+        public
+        view
+        returns (string memory)
+    {
+        string memory question = getPropsForId(tokenId).question;
+        bytes memory svg = abi.encodePacked(
+            '<svg xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMinYMin meet" viewBox="0 0 350 350">',
+            "<style>.base { fill: white; font-family: serif; font-size: 14px; }</style>",
+            '<rect width="100%" height="100%" fill="black" />',
+            '<text x="50%" y="40%" class="base" dominant-baseline="middle" text-anchor="middle">',
+            "Warrior",
+            "</text>",
+            '<text x="50%" y="50%" class="base" dominant-baseline="middle" text-anchor="middle">',
+            "Question: ",
+            question,
+            "</text>",
+            "</svg>"
         );
-
-        // console.log(tokenId, idToListedToken[tokenId].owner);
+        return
+            string(
+                abi.encodePacked(
+                    "data:image/svg+xml;base64,",
+                    Base64.encode(svg)
+                )
+            );
     }
 
-    function getListedTokenForId(uint256 tokenId) public view returns (ListedToken memory) {
-        return idToListedToken[tokenId];
+    function getTokenURI(uint256 tokenId) public view returns (string memory) {
+        bytes memory dataURI = abi.encodePacked(
+            "{",
+            '"name": "Question #',
+            tokenId.toString(),
+            '",',
+            '"description": "question description",',
+            '"image": "',
+            generateImage(tokenId),
+            '"',
+            "}"
+        );
+        return
+            string(
+                abi.encodePacked(
+                    "data:application/json;base64,",
+                    Base64.encode(dataURI)
+                )
+            );
     }
 
+    /**
+     * @dev function about answering
+     */
+    function setAnswer(uint256 tokenId, string memory answerText) public {
+        uint256 answerId = _answerIdCounter.current();
+        _answerIdCounter.increment();
+        Answer memory _answer = Answer(tokenId, answerId, payable(msg.sender), answerText);
+        idToAnswers[tokenId].answers.push(_answer);
+    }
+
+    function getAnswers(uint256 tokenId) public view returns (Answer[] memory) {
+        console.log(
+            "sender",
+            idToAnswers[tokenId].answers[0].sender,
+            idToAnswers[tokenId].answers[1].sender
+        );
+        return idToAnswers[tokenId].answers;
+    }
+
+    /**
+     * @dev helper function
+     */
     // The following functions are overrides required by Solidity.
 
-    function _beforeTokenTransfer(address from, address to, uint256 tokenId)
-        internal
-        override(ERC721, ERC721Enumerable)
-    {
+    function _beforeTokenTransfer(
+        address from,
+        address to,
+        uint256 tokenId
+    ) internal override(ERC721, ERC721Enumerable) {
         super._beforeTokenTransfer(from, to, tokenId);
     }
 
-    function _burn(uint256 tokenId) internal override(ERC721, ERC721URIStorage) {
+    function _burn(uint256 tokenId)
+        internal
+        override(ERC721, ERC721URIStorage)
+    {
         super._burn(tokenId);
     }
 
--- test/TestNft.js
+++ test/TestNft.js
@@ -3,14 +3,18 @@ const { ethers } = require("hardhat");
 
 describe("CAS contract", function () {
   it("NFT mint", async function () {
-    const [owner] = await ethers.getSigners();
+    const [owner, addr1, addr2] = await ethers.getSigners();
+    console.log("addresses", owner.address, addr1.address, addr2.address);
 
     const CoAnswer = await ethers.getContractFactory("CoAnswer");
 
     const CASContract = await CoAnswer.deploy();
     await CASContract.deployed();
 
-    await CASContract.safeMint(owner.address, "https://hardhat.org/tutorial/testing-contracts", { value: ethers.utils.parseEther("0.001") });
+    await CASContract.safeMint(owner.address, "what is your name ?", { value: ethers.utils.parseEther("0.001") });
+    await CASContract.connect(addr1).setAnswer(0, "kyok");
+    await CASContract.connect(addr2).setAnswer(0, "taro");
+    await CASContract.getAnswers(0);
     expect(await CASContract.ownerOf(0)).to.equal(owner.address);
   });
 });
